9eed73a6655681026b58d480a58ec90f5766fa30
brennon@brennonbortz.com <brennon@brennonbortz.com>
2018-07-01 22:44:07 +0000

    Add additional features

--- Hallelujah_Effect/taxifare_tft/trainer/model.py
+++ Hallelujah_Effect/taxifare_tft/trainer/model.py
@@ -12,91 +12,79 @@ from tensorflow_transform.tf_metadata import metadata_io
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-CSV_COLUMNS = 'age,activity,hallelujah_reaction'.split(',')
+# CSV_COLUMNS = 'age,activity,hallelujah_reaction'.split(',')
 LABEL_COLUMN = 'hallelujah_reaction'
 KEY_FEATURE_COLUMN = None
 # DEFAULTS = [[0.0], ['Sun'], [0], [-74.0], [40.0], [-74.0], [40.7], [1.0], ['nokey']]
 
 # These are the raw input columns, and will be provided for prediction also
 INPUT_COLUMNS = [
-    # Define features
-    # tf.feature_column.categorical_column_with_identity('dayofweek', num_buckets = 100 ),  # some large number
-    # tf.feature_column.categorical_column_with_identity('hourofday', num_buckets = 24),
-    # tf.feature_column.categorical_column_with_identity('hallelujah_reaction'),
-    tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('location', ['dublin', 'taipei_city', 'taichung_city'])),
+    # Categorical features
     tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('language', ['N/A', 'en', 'zh_TW'])),
-    tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('sex', ['female', 'male'])),
+    tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('location', ['dublin', 'taipei_city', 'taichung_city'])),
     tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('nationality', ['other', 'irish', 'british', 'taiwanese', 'indonesian', 'japanese', 'chinese', 'singaporean', 'american', 'algerian', 'thai'])),
+    tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('sex', ['female', 'male'])),
 
-    # Numeric columns
-    # tf.feature_column.numeric_column('pickuplon'),
-    # tf.feature_column.numeric_column('pickuplat'),
-    # tf.feature_column.numeric_column('dropofflat'),
-    # tf.feature_column.numeric_column('dropofflon'),
-    # tf.feature_column.numeric_column('passengers'),
+    # Numeric features
     tf.feature_column.numeric_column('activity'),
+    tf.feature_column.numeric_column('artistic'),
     tf.feature_column.numeric_column('age'),
-    tf.feature_column.numeric_column('hearing_impairments'),
+    tf.feature_column.numeric_column('concentration'),
     tf.feature_column.numeric_column('engagement'),
     tf.feature_column.numeric_column('familiarity'),
+    tf.feature_column.numeric_column('fault'),
+    tf.feature_column.numeric_column('hearing_impairments'),
+    tf.feature_column.numeric_column('imagination'),
+    tf.feature_column.numeric_column('lazy'),
     tf.feature_column.numeric_column('like_dislike'),
+    tf.feature_column.numeric_column('musical_expertise'),
+    tf.feature_column.numeric_column('music_pref_classical'),
+    tf.feature_column.numeric_column('music_pref_dance'),
+    tf.feature_column.numeric_column('music_pref_folk'),
+    tf.feature_column.numeric_column('music_pref_hiphop'),
+    tf.feature_column.numeric_column('music_pref_jazz'),
+    tf.feature_column.numeric_column('music_pref_none'),
+    tf.feature_column.numeric_column('music_pref_pop'),
+    tf.feature_column.numeric_column('music_pref_rock'),
+    tf.feature_column.numeric_column('music_pref_traditional_irish'),
+    tf.feature_column.numeric_column('music_pref_world'),
+    tf.feature_column.numeric_column('nervous'),
+    tf.feature_column.numeric_column('outgoing'),
     tf.feature_column.numeric_column('positivity'),
-    tf.feature_column.numeric_column('tension')
+    tf.feature_column.numeric_column('reserved'),
+    tf.feature_column.numeric_column('stress'),
+    tf.feature_column.numeric_column('tension'),
+    tf.feature_column.numeric_column('thorough'),
+    tf.feature_column.numeric_column('trusting')
     
     # Engineered features that are created in the input_fn
-    # tf.feature_column.numeric_column('latdiff'),
-    # tf.feature_column.numeric_column('londiff'),
-    # tf.feature_column.numeric_column('euclidean')
 ]
 
 # Build the estimator
 def build_estimator(model_dir, nbuckets, hidden_units):
     """
-     Build an estimator starting from INPUT COLUMNS.
-     These include feature transformations and synthetic features.
-     The model is a wide-and-deep model.
-  """
+    Build an estimator starting from INPUT COLUMNS.
+    These include feature transformations and synthetic features.
+    The model is a wide-and-deep model.
+    """
 
     # Input columns
-    # (dayofweek, hourofday, latdiff, londiff, euclidean, plon, plat, dlon, dlat, pcount) = INPUT_COLUMNS
-    (location, language, sex, activity, age, hearing_impairments, nationality, engagement, familiarity, like_dislike, positivity, tension) = INPUT_COLUMNS
+    (language, location, nationality, sex, activity, artistic, age, concentration, engagement, familiarity, fault, hearing_impairments, imagination, lazy, like_dislike, musical_expertise, music_pref_classical, music_pref_dance, music_pref_folk, music_pref_hiphop, music_pref_jazz, music_pref_none, music_pref_pop, music_pref_rock, music_pref_traditional_irish, music_pref_world, nervous, outgoing, positivity, reserved, stress, tension, thorough, trusting) = INPUT_COLUMNS
 
-    # Bucketize the lats & lons
-    # latbuckets = np.linspace(0, 1.0, nbuckets).tolist()
-    # lonbuckets = np.linspace(0, 1.0, nbuckets).tolist()
-    # b_plat = tf.feature_column.bucketized_column(plat, latbuckets)
-    # b_dlat = tf.feature_column.bucketized_column(dlat, latbuckets)
-    # b_plon = tf.feature_column.bucketized_column(plon, lonbuckets)
-    # b_dlon = tf.feature_column.bucketized_column(dlon, lonbuckets)
-
-    # Feature cross
-    # ploc = tf.feature_column.crossed_column([b_plat, b_plon], nbuckets * nbuckets)
-    # dloc = tf.feature_column.crossed_column([b_dlat, b_dlon], nbuckets * nbuckets)
-    # pd_pair = tf.feature_column.crossed_column([ploc, dloc], nbuckets ** 4 )
-    # day_hr =  tf.feature_column.crossed_column([dayofweek, hourofday], 24 * 7)
-
-    # Wide columns and deep columns.
+    # Wide columns and deep columns
     wide_columns = [
         # Feature crosses
-        # dloc, ploc, pd_pair,
-        # day_hr,
 
         # Sparse columns
-        # dayofweek, hourofday,
 
         # Anything with a linear relationship
-        # pcount 
     ]
 
     deep_columns = [
-        # Embedding_column to "group" together ...
-        # tf.feature_column.embedding_column(pd_pair, 10),
-        # tf.feature_column.embedding_column(day_hr, 10),
+        # Embedding columns
 
         # Numeric columns
-        # plat, plon, dlat, dlon,
-        # latdiff, londiff, euclidean
-        location, language, sex, activity, age, hearing_impairments, nationality, engagement, familiarity, like_dislike, positivity, tension
+        language, location, nationality, sex, activity, artistic, age, concentration, engagement, familiarity, fault, hearing_impairments, imagination, lazy, like_dislike, musical_expertise, music_pref_classical, music_pref_dance, music_pref_folk, music_pref_hiphop, music_pref_jazz, music_pref_none, music_pref_pop, music_pref_rock, music_pref_traditional_irish, music_pref_world, nervous, outgoing, positivity, reserved, stress, tension, thorough, trusting
     ]
     
     checkpointing_config = tf.estimator.RunConfig(
@@ -104,38 +92,26 @@ def build_estimator(model_dir, nbuckets, hidden_units):
         keep_checkpoint_max = 10,       # Retain the 10 most recent checkpoints.
     )
     
-    return tf.estimator.DNNLinearCombinedClassifier(
+    estimator = tf.estimator.DNNLinearCombinedClassifier(
         model_dir = model_dir,
         linear_feature_columns = wide_columns,
         dnn_feature_columns = deep_columns,
         dnn_hidden_units = hidden_units,
         config=checkpointing_config)
+    
+    # estimator = tf.contrib.estimator.add_metrics(estimator, add_eval_metrics)
+    return estimator
 
 # Create serving input function to be able to serve predictions
-def make_serving_input_fn_for_base64_json(args):
-    raw_metadata = metadata_io.read_metadata(
-        os.path.join(args['metadata_path'], 'rawdata_metadata'))
-    transform_savedmodel_dir = (
-        os.path.join(args['metadata_path'], 'transform_fn'))
-    return input_fn_maker.build_parsing_transforming_serving_input_receiver_fn(
-      raw_metadata,
-      transform_savedmodel_dir,
-      exclude_raw_keys = [LABEL_COLUMN])
-
 def make_serving_input_fn(args):
   transform_savedmodel_dir = (
         os.path.join(args['metadata_path'], 'transform_fn'))
 
   def _input_fn():
-    # placeholders for all the raw inputs
+    # Placeholders for all the raw inputs; a lot of inputs are missing here
     feature_placeholders = {
-      # column_name: tf.placeholder(tf.float32, [None]) for column_name in 'pickuplon,pickuplat,dropofflat,dropofflon'.split(',')
       column_name: tf.placeholder(tf.float32, [None]) for column_name in 'age,activity'.split(',')
     }
-    # feature_placeholders['passengers'] = tf.placeholder(tf.int64, [None])
-    # feature_placeholders['dayofweek'] = tf.placeholder(tf.string, [None])
-    # feature_placeholders['hourofday'] = tf.placeholder(tf.int64, [None])
-    # feature_placeholders['key'] = tf.placeholder(tf.string, [None])
 
     # transform using the saved model in transform_fn
     _, features = saved_transform_io.partially_apply_saved_transform(
@@ -155,18 +131,18 @@ def read_dataset(args, mode):
         input_paths = args['eval_data_paths']
     
     transformed_metadata = metadata_io.read_metadata(
-              os.path.join(args['metadata_path'], 'transformed_metadata'))
+        os.path.join(args['metadata_path'], 'transformed_metadata'))
 
     return input_fn_maker.build_training_input_fn(
-          metadata = transformed_metadata,
-          file_pattern = (
-              input_paths[0] if len(input_paths) == 1 else input_paths),
-          training_batch_size = batch_size,
-          label_keys = [LABEL_COLUMN],
-          reader = gzip_reader_fn,
-          key_feature_name = KEY_FEATURE_COLUMN,
-          randomize_input = (mode != tf.estimator.ModeKeys.EVAL),
-          num_epochs = (1 if mode == tf.estimator.ModeKeys.EVAL else None)) 
+        metadata = transformed_metadata,
+        file_pattern = (
+          input_paths[0] if len(input_paths) == 1 else input_paths),
+        training_batch_size = batch_size,
+        label_keys = [LABEL_COLUMN],
+        reader = gzip_reader_fn,
+        key_feature_name = KEY_FEATURE_COLUMN,
+        randomize_input = (mode != tf.estimator.ModeKeys.EVAL),
+        num_epochs = (1 if mode == tf.estimator.ModeKeys.EVAL else None)) 
 
 # Create estimator train and evaluate function
 def train_and_evaluate(args):
@@ -189,8 +165,8 @@ def gzip_reader_fn():
     return tf.TFRecordReader(options=tf.python_io.TFRecordOptions(
             compression_type = tf.python_io.TFRecordCompressionType.GZIP))
 
-def get_eval_metrics():
+def add_eval_metrics(labels, predictions):
+    # pred_values = predictions['predictions']
     return {
-        'rmse': tflearn.MetricSpec(metric_fn=metrics.streaming_root_mean_squared_error),
-        'training/hptuning/metric': tflearn.MetricSpec(metric_fn=metrics.streaming_root_mean_squared_error),
+        'confusion_matrix': tf.confusion_matrix(labels, predictions)
     }
